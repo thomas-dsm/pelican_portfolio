23a357318be90d336fa9dbc7dfbfe210a1d83f06
Thomas Da Silva Mendonca <thomas.dasilvamendonca@protonmail.com>
2022-09-18 18:36:37 +0200

    ajout de tous le site

--- content/post.md
+++
@@ -1,9 +0,0 @@
-Title: Welcome All
-Slug: welcome-all
-Date: 2013-07-22 19:19
-Category: Python
-Tags: pelican, publishing
-Author: Josef Jezek
-Summary: Short version for index and feeds
-
-This is the content of my blog post.
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Thomas Da Silva Mendonca'
-SITENAME = 'Portfolio'
+AUTHOR = 'DA SILVA MENDONCA Thomas'
+SITENAME = 'portfolio_DaSilvaMendonca'
 SITEURL = ''
+LOGO = 'teemtho_Crosscode'
 
 PATH = 'content'
 
@@ -29,13 +30,52 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+#RELATIVE_URLS = TrueTHEME = 'themes/mon-theme-pelican'
 
-OUTPUT_PATH = 'output'
-PATH = 'content'
+THEME = 'themes/mon-theme-pelican'
+CSS_FILE = 'portfolio.css'
 
-ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
+MENU = (
+	('#apropos', 'À Propos'),
+    ('#cursus', 'Cursus'),
+    ('#competences','Competences'),
+	('#contributions', 'Contributions'),
+	('#contact', 'Contact')
+)
+
+
+COMPETENCE1 = (
+(('PHP (en travaillant sur des projets professionels en Symfony et Thelia)'),('80%')),
+(('WEB (HTML, CSS, Bootstrap)'),('70%')),
+(('Java et JavaFX'),('70%')),
+(('Python (avec Tkinter)'), ('60%')),
+)
+
+COMPETENCE2 = (
+(('L\'Autonomie'),('J\'ai travaillé en autonomie sur le projets SAP où on me demandé d\'integrer des nouvelles technologies dans mon projets comme SASS')),
+(('Le Travail en équipe'),(' Dans le cadre de ma formation en BTS, j\'ai été amené à gerer une équipe pour le developpement d\'une application web sous symfony et une autre en JavaFX')),
+(('La Curiosité'),('je m\'interresse aux nouvelles technologies comme aux I.A. (l\'image de profil a été gener par Midjourney)')),
+)
+
+COMPETENCE3 = (
+('Niveau d\'Anglais : B2'),
+)
+
+
+# Formulaire de contact
+TITRE_CONTACT = 'Contactez moi'
+NOM_FORM = 'Formulaire de contact'
+CHAMPS_CONTACT = (
+	('Nom', 'text', 'nom', 'Entrez votre nom.'),
+	('E-mail', 'email', 'email','Saisissez votre adresse e-mail.' ),
+	('Message', 'textarea', 'message', 'Saisissez votre message.')
+)
+
+OUTPUT_PATH = 'public'
+
+
+
+
